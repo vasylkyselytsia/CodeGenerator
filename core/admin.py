0e8930873f365de267acd35a002a6f0cb36074e6
VasokKiselicya <v.kiselicya021036@gmail.com>
2018-04-01 13:53:26 +0300

    Add Code Template Saving

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth import models as auth_models
-# from django.template.loader import render_to_string
+from django.conf.urls import url
+from django.urls import reverse
+from django.utils.html import format_html
+from django.template.response import TemplateResponse
 
 from core import models
 
@@ -37,7 +40,7 @@ class LanguageAdmin(RemovePermissionMixin, admin.ModelAdmin):
     search_fields = ("name",)
     list_display = ("name",)
     readonly_fields = list_display
-    icon = '<i class="material-icons">settings</i>'
+    icon = '<i class="material-icons">language</i>'
 
 
 @admin.register(models.Function)
@@ -45,7 +48,7 @@ class FunctionAdmin(admin.ModelAdmin):
     search_fields = ("name", "value")
     list_display = ("language", "name", "value",)
     list_filter = ("language",)
-    icon = '<i class="material-icons">settings</i>'
+    icon = '<i class="material-icons">build</i>'
     ordering = ("language", "value")
 
 
@@ -54,8 +57,55 @@ class KeywordAdmin(admin.ModelAdmin):
     search_fields = ("name", "value")
     list_display = ("language", "name", "value",)
     list_filter = ("language",)
-    icon = '<i class="material-icons">settings</i>'
+    icon = '<i class="material-icons">vpn_key</i>'
     ordering = ("language", "value")
+
+
+class AddOnesInline(admin.StackedInline):
+    extra = 1
+    fields = ('name', 'v_type', 'default')
+    model = models.AddOnes
+
+
+class AddOnesFuncInline(admin.StackedInline):
+    extra = 1
+    fields = ('name', 'is_friend', 'params')
+    model = models.FuncAddOnes
+
+
+@admin.register(models.CodeTemplate)
+class CodeTemplateAdmin(RemovePermissionMixin, admin.ModelAdmin):
+    CLOSED_PERMISSIONS = ["delete"]
+    search_fields = ("language__name", "name")
+    list_display = ("language", "name", "create_dt", "view_actions")
+    icon = '<i class="material-icons">code</i>'
+    inlines = [AddOnesInline, AddOnesFuncInline]
+
+    def process_view(self, request, template_id):
+        code = self.get_object(request, template_id)
+        context = self.admin_site.each_context(request)
+        context['code'] = code
+        return TemplateResponse(request, 'code_view.html', context)
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            url(
+                r'^(?P<template_id>.+)/view-code/$',
+                self.admin_site.admin_view(self.process_view),
+                name='view-code',
+            )
+        ]
+        return custom_urls + urls
+
+    def view_actions(self, obj):
+        return format_html(
+            '<a class="button" href="{}">Згенерувати код</a>',
+            reverse('admin:view-code', args=[obj.pk])
+        )
+
+    view_actions.short_description = 'Дії'
+    view_actions.allow_tags = True
 
 
 admin.site.unregister(auth_models.Group)
--- core/models.py
+++ core/models.py
@@ -45,3 +45,51 @@ class Keyword(models.Model):
         verbose_name = "Ключове слово"
         verbose_name_plural = "Ключові слова"
         unique_together = (("language", "value"),)
+
+
+class CodeTemplate(models.Model):
+    language = models.ForeignKey(Language, null=False, related_name="code_templates",
+                                 related_query_name="code_templates", verbose_name="Мова програмування")
+    name = models.CharField(max_length=30, verbose_name="Назва класу")
+    create_dt = models.DateTimeField(auto_now_add=True, verbose_name="Дата сторення")
+
+    class Meta:
+        db_table = "app_code_template"
+        verbose_name = "Шаблон коду"
+        verbose_name_plural = "Шаблони коду"
+
+    def __str__(self):
+        return "{} => {} [{}]".format(self.language, self.name, self.create_dt.strftime('%Y-%m-%d %H:%M'))
+
+
+class AddOnes(models.Model):
+    TYPES = (
+        ("int", "Integer"),
+        ("float", "Real"),
+        ("str", "String"),
+        ("bool", "Boolean")
+    )
+
+    template = models.ForeignKey(CodeTemplate, null=False, related_name="add_ones",
+                                 related_query_name="add_ones", verbose_name="Мова програмування")
+    name = models.CharField(max_length=30, verbose_name="Назва")
+    v_type = models.CharField(max_length=30, choices=TYPES, verbose_name="Тип")
+    default = models.CharField(max_length=100, default=None, verbose_name="Значення")
+
+    class Meta:
+        db_table = "app_add_ones"
+        verbose_name = "Змінна"
+        verbose_name_plural = "Змінні"
+
+
+class FuncAddOnes(models.Model):
+    template = models.ForeignKey(CodeTemplate, null=False, related_name="add_ones_func",
+                                 related_query_name="add_ones_func", verbose_name="Мова програмування")
+    name = models.CharField(max_length=30, verbose_name="Назва")
+    is_friend = models.BooleanField(default=False, verbose_name="Дружня")
+    params = models.TextField(max_length=100, default="", verbose_name="Параметри [a,b,c=2]", blank=True)
+
+    class Meta:
+        db_table = "app_add_ones_func"
+        verbose_name = "Функція"
+        verbose_name_plural = "Функції"
