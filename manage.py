c7bea45d3af4011b3b3cbbf21f5b32b64df34b8e
VasokKiselicya <v.kiselicya021036@gmail.com>
2018-04-09 14:43:03 +0300

    Upload To Heroku

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from utils import Util
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CodeGenerator.settings")
-    Util.run()
+    Util().run()
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
